748695d9fc2b439241ee0fa1b27614b9af984de8
Ren Jing <reni2046@gmail.com>
2013-01-16 13:24:04 +0800

    cabinet: add a test for cabinet storable? method

--- test/cabinet_test.rb
+++ test/cabinet_test.rb
@@ -2,13 +2,18 @@ require "minitest/autorun"
 require "../src/cabinet"
 
 class CabinetTest < MiniTest::Unit::TestCase
-  def test_should_return_available_box_number
+  def test_available_boxes_should_return_available_box_number
     cabinet = Cabinet.new(1)
     assert_equal 1, cabinet.available_boxes
   end
 
-  def test_should_tell_whether_can_store_bag
+  def test_should_tell_cabinet_can_store_bag
     cabinet = Cabinet.new(1)
     assert cabinet.storable?
+  end
+
+  def test_should_tell_cabinet_can_not_store_bag
+    cabinet = Cabinet.new(0)
+    assert !cabinet.storable?
   end
 end
